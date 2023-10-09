7c032b5a0607ac7675f2a73baf56334f80de3ebf
Eduardo Coutinho Idalgo <eduardoidalgo7@gmail.com>
2023-10-09 17:13:22 -0300

    Aula 5 adicionada

---
+++ aula05-constantes_const/index.js
@@ -0,0 +1,13 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// camelCase
+// Case-sensitive (Letra minúscula != Letra maiúscula)
+// Não pode modificar o valor de uma constante
+// NÃO UTILIZAR VAR, UTILIZE CONST!
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+console.log(typeof resultado);
