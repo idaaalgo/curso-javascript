f4ffcd2f66cf04643dd9b810de8fdbd957a25c6f
Eduardo Coutinho Idalgo <eduardoidalgo7@gmail.com>
2023-10-09 18:23:13 -0300

    Resolução do desafio proposto na aula

--- desafios/exercicio_let_const/index.js
+++ desafios/exercicio_let_const/index.js
@@ -1,6 +1,6 @@
 const nome = 'Eduardo';
 const sobrenome = 'Idalgo';
-const nomeCompleto  = nome + " " +sobrenome;
+const nomeCompleto  = nome + " " + sobrenome;
 const idade = 19;
 const alturaEmMetros = 1.81;
 const pesoEmKg = 72;
@@ -9,6 +9,6 @@ const anoAtual = dataAtual.getFullYear();
 const imc = pesoEmKg / Math.pow(alturaEmMetros, 2);
 const anoDeNascimento = anoAtual - idade;
 
-console.log(nomeCompleto, "Tem", idade, 'anos, pesa', pesoEmKg, 'Kg');
-console.log('Tem', alturaEmMetros.toFixed(2), 'de altura e seu IMC é de', imc.toFixed(2));
-console.log(nome, 'nasceu em', anoDeNascimento);
+console.log(`${nomeCompleto} Tem ${idade} anos, pesa ${pesoEmKg}Kg`);
+console.log(`Tem ${alturaEmMetros.toFixed(2)} de altura e seu IMC é de ${imc.toFixed(2)}`);
+console.log(`${nome} nasceu em ${anoDeNascimento}`);
