5f66b339f11cb26c92c1a720c80eeec929d291a4
Eduardo Coutinho Idalgo <eduardoidalgo7@gmail.com>
2023-10-09 18:17:13 -0300

    Criação da pasta aulas e desafios, e resolução do desafio

---
+++ desafios/exercicio_let_const/index.js
@@ -0,0 +1,14 @@
+const nome = 'Eduardo';
+const sobrenome = 'Idalgo';
+const nomeCompleto  = nome + " " +sobrenome;
+const idade = 19;
+const alturaEmMetros = 1.81;
+const pesoEmKg = 72;
+const dataAtual = new Date();
+const anoAtual = dataAtual.getFullYear();
+const imc = pesoEmKg / Math.pow(alturaEmMetros, 2);
+const anoDeNascimento = anoAtual - idade;
+
+console.log(nomeCompleto, "Tem", idade, 'anos, pesa', pesoEmKg, 'Kg');
+console.log('Tem', alturaEmMetros.toFixed(2), 'de altura e seu IMC é de', imc.toFixed(2));
+console.log(nome, 'nasceu em', anoDeNascimento);
