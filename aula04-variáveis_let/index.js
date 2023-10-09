ff37888c621d58a2c9256a52d540d95a4bd6cd33
Eduardo Coutinho Idalgo <eduardoidalgo7@gmail.com>
2023-10-09 14:28:42 -0300

    Aula 04 atualizada

--- aula04-variáveis_let/index.js
+++ aula04-variáveis_let/index.js
@@ -1,8 +1,11 @@
-let nome = 'João';
+// Não podemos criar variáveis com palavras reservadas
+// let let; // ERRADO
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// camelCase
+// Case-sensitive (Letra minúscula != Letra maiúscula)
+let nomeCliente = 'Eduardo';
+let nomecliente = 'Idalgo';
 
-console.log(nome, 'nasceu em 1984.'); // concatenando com + ou com , funcionam normalmente
-// console.log(nome + ' nasceu em 1984.'); 
-console.log('Em 2000', nome, 'conheceu Maria.');
-console.log(nome, 'casou-se com Maria em 2012.');
-console.log('Maria teve 1 filho com', nome, 'em 2015.');
-console.log('O filho de', nome, 'se chama Eduardo.');
+console.log(nomeCliente, nomecliente);
